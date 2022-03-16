f0514b8f8f020178bd977d312e4baede211d691e
wbj0t <theonetwothe@gmail.com>
2022-03-16 15:58:04 +0400

    Refactor and rebuild webpack

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,1 +1,21 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/tailwind.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+</head>
+<body>
+
+@include('layouts.header')
+
+@include('layouts.footer')
+
+<script src="assets/js/main.js"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,24 +1,26 @@
 const mix = require('laravel-mix');
 
 mix.scripts([
-    'resources/js/bootstrap/bootstrap.bundle.js',
-    'resources/js/main.js'
-], 'public/js/main.js')
+    'resources/assets/js/bootstrap/bootstrap.bundle.js',
+    'resources/assets/js/main.js'
+], 'public/assets/js/main.js')
 
 mix.styles([
-    'resources/css/bootstrap/bootstrap.css',
-], 'public/css/main.css')
+    'resources/assets/css/bootstrap/bootstrap.css',
+], 'public/assets/css/main.css')
 
 mix.copy([
-    'resources/js/bootstrap/bootstrap.bundle.js.map'
-], 'public/js/bootstrap.bundle.js.map')
+    'resources/assets/js/bootstrap/bootstrap.bundle.js.map'
+], 'public/assets/js/bootstrap.bundle.js.map')
 
 mix.copy([
-    'resources/css/bootstrap/bootstrap.css.map'
-], 'public/css/bootstrap.css.map')
+    'resources/assets/css/bootstrap/bootstrap.css.map'
+], 'public/assets/css/bootstrap.css.map')
+
+mix.copyDirectory('resources/assets/img/used-icons', 'public/assets/img/icons');
 
 /*.js('resources/js/app.js', 'public/js')*/
-mix.postCss('resources/css/tailwind.css', 'public/css', [
+mix.postCss('resources/assets/css/tailwind.css', 'public/assets/css', [
         require("tailwindcss"),
     ])
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/assets/css');
